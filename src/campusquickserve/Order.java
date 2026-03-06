975f3b29a03028ec1e613ee777806fd705330d7f
Da Zhaun Thorne <St10471460@myemeris.edu.za>
2026-03-06 17:26:44 +0200

    3rd commit test created receipt

--- src/campusquickserve/Order.java
+++ src/campusquickserve/Order.java
@@ -3,14 +3,15 @@ package campusquickserve;
 import java.util.Scanner;
 
 public class Order {
-    
+  //creating the fields  
  private String CustomerName;
  private String StudentNumber;
  private String ItemName; 
  private double baseprice; 
  private final double VATrate = 0.15;
  private double calculateVAT;
-    
+ 
+ //making the constructor
 public Order(String name, String studentNumber, String item, double price){
     this.CustomerName = name;
     this.StudentNumber = studentNumber;
@@ -26,7 +27,29 @@ public double calculateVAT(){
 
 //Calc total
 public double Total(){
-    return baseprice + calculateVAT;
+    return baseprice + calculateVAT;    
+}
+
+//creating the reciept
+public String Reciept(){
+    double vatAmount = calculateVAT();
+    double totalAmount = Total();
+
+return "-----------QuickServe Reciept-----------\n" +
+        "Customer Name:" + CustomerName + "\n" +
+        "Student Name:" + StudentNumber + "\n" +
+        "Item Ordered:" + ItemName + "\n" +
+        "Price: R" + baseprice + "\n" +
+        "VAT (15%):" + vatAmount + "\n" +
+        "Total: R" + totalAmount + "\n" + "----------------------------------------";
+
+        
+        
+        
+        
+        
+    
+
 
 }
 
